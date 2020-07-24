80375047bfa69c5fbbe7caeb7bfa1a0acea97ea0
nullbitcoder <lxxxl1146@gmail.com>
2020-07-24 15:01:05 -0500

    clean up

--- vue-login/src/App.vue
+++ vue-login/src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <v-app>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
         <v-img
           alt="Vuetify Name"
