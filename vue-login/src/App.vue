6e079db521974bb26619c63e24a0d1bb5ec31a94
nullbitcoder <lxxxl1146@gmail.com>
2020-07-24 15:33:08 -0500

    build out

--- vue-login/src/App.vue
+++ vue-login/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
     <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
   </v-app>
 </template>
 
