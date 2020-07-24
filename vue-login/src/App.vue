dc08f0dbfdccd5cdcf85a6ed5584eaf964faa576
nullbitcoder <lxxxl1146@gmail.com>
2020-07-24 15:02:36 -0500

    clean up

--- vue-login/src/App.vue
+++ vue-login/src/App.vue
@@ -2,14 +2,7 @@
   <v-app>
       <div class="d-flex align-center">
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+       
       </div>
 
       <v-spacer></v-spacer>
